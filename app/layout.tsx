414ce7793d42c5fc56bf3bffa6c7c7fdd3efa8a8
Mohd arshad khan <sameer7037k@gamil.com>
2025-04-18 16:21:10 +0530

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Navbar from './components/Navbar'; // ✅ Import your Navbar
+ // ✅ Import your Navbar
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-black`}>
-        <Navbar /> {/* ✅ Shows Navbar on every page */}
+        {/* <Navbar /> ✅ Shows Navbar on every page */ }
+        
         {children}
       </body>
     </html>
   );
 }
+
+//github change 1
