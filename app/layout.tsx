cdfbe4aae6a7ce55672ee1df5dc637f4c3bead74
Mohd arshad khan <sameer7037k@gamil.com>
2025-04-18 16:27:11 +0530

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Liqwid',
   description: 'Custom login and dashboard',
+  icons: {
+    icon: '/fevicon.ico', //
+  },
 };
 
 export default function RootLayout({
